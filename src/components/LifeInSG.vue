b860bc6df52ff030018e1d9388d1a967ae4ddb62
洪念綺 <jennyhung1212@gmail.com>
2019-02-07 10:51:32 +0800

    correct wordings, update favicon

--- src/components/LifeInSG.vue
+++ src/components/LifeInSG.vue
@@ -16,7 +16,7 @@
 	  		</div>
 	  		<div class="content__content">
 	  			<div class="content__content--text">
-	  				Firist week is a week full of new things: new culture, new people, new dorm, new campus. This is my first time going abroad alone. I always joked about that flying to Singapore takes as long as taking a bus from Taipei to Tainan, it's nothing! The truth is I'm still a little nervous, not because I'm far away from home, but because this is the "learning vacation" I promised myself before stepping into the workplace, and I'm afraid that I might not live the full of it.
+	  				First week is a week full of new things: new culture, new people, new dorm, new campus. This is my first time going abroad alone. I always joked about that flying to Singapore takes as long as taking a bus from Taipei to Tainan, it's nothing! The truth is I'm still a little nervous, not because I'm far away from home, but because this is the "learning vacation" I promised myself before stepping into the workplace, and I'm afraid that I might not live the full of it.
 	  				<br><br>
 	  				The next two days after I arrived in SUTD is Matriculation and Orientation Day. There are around 20 exchange students this term. Together we listened to the introduction of school facilities, insurance policies, had a tour around campus and the beautiful library. Which amazed me the most is the amount of equiments in cohort classroom and fab lab. It's crazy that a small school like this can have that many equiments for students to use! The sports center and the swimmping pool are also quite stunning!
 	  				<br><br>
@@ -32,7 +32,7 @@
 	  				<div class="horizontal-double__annotation">1) The view from Chinatown Foodcourt. 2) My Amazing Race teammates.</div>
 	  			</div>
 	  			<div class="content__content--text">
-	  				Next, we went to habourfront station. We visited helix bridge, the shoppes at Marina Bay Sands, and Garden by the Bay. The Marina Bay Sands is really spectacular when watching it closely, and the garden is also very splendid. It's like the reproduce of the scene in the movie "Cray Rich Asians". Finally we took MRT to Dhoby Ghaut station, and visited Singapore Art Museum, National Museum of Singapore, Chijmas, St. Andrews Cathedral, and finally the MacDonald House, which was the first building with air-conditioner in Southeast Asia. We took lots of great pictures and got to know Singaporean history and environment more.
+	  				Next, we went to habourfront station. We visited helix bridge, the shoppes at Marina Bay Sands, and Garden by the Bay. The Marina Bay Sands is really spectacular when watching it closely, and the garden is also very splendid. It's like the reproduce of the scene in the movie "Crazy Rich Asians". Finally we took MRT to Dhoby Ghaut station, and visited Singapore Art Museum, National Museum of Singapore, Chijmas, St. Andrews Cathedral, and finally the MacDonald House, which was the first building with air-conditioner in Southeast Asia. We took lots of great pictures and got to know Singaporean history and environment more.
 	  			</div>
 	  			<div class="content__content--image horizontal-single">
 	  				<div class="horizontal-single__frame">
@@ -41,7 +41,7 @@
 	  				<div class="horizontal-single__annotation">National Museum of Singapore, built in 1849.</div>
 	  			</div>
 	  			<div class="content__content--text">
-	  				The other worth-mentioning event is the first CIP activity arranged by TFI LEaRN Programme. Me, Kwangho and Ronald went to Redhill to help distributing food to needy families. The organization that hold the food distributing event is Apex Club of Bukit Timah. We took food, 紅包 and other daily supplies to the families on the list and whish them a happy Chinese new year. Although Singapore is one of the most modern and wealthy cities in the world, there are still many people struggling to live. There's nothing better then the smiles and the thankfulness we received on that hot Saturday afternnon. 施比受更有福！
+	  				The other worth-mentioning event is the first CIP activity arranged by TFI LEaRN Programme. Me, Kwangho and Ronald went to Redhill to help distributing food to needy families. The organization that hold the food distributing event is Apex Club of Bukit Timah. We took food, 紅包 and other daily supplies to the families on the list and wish them a happy Chinese new year. Although Singapore is one of the most modern and wealthy cities in the world, there are still many people struggling to live. There's nothing better then the smiles and the thankfulness we received on that hot Saturday afternnon. 施比受更有福！
 	  			</div>
 	  			<div class="content__content--image mixed-triple">
 	  				<div class="inline-block">
