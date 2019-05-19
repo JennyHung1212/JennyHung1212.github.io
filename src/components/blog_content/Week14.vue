030b1c0dea26244213346cd5d04533daed854a77
洪念綺 <jennyhung1212@gmail.com>
2019-05-19 15:49:35 +0800

    week 14 done

--- src/components/blog_content/Week14.vue
+++ src/components/blog_content/Week14.vue
@@ -9,9 +9,11 @@
 		<div class="content__content">
 			<div class="content__content--text">
 				<p class="subtitle">Final Presentations for ESA and OP</p>
+				I only took two courses this term, one is Engineering System Architecture, and the other is Operational Process. The way professors give lectures is very different from Taiwan. The courses in SUTD require lots of in-class activities and discussions, and the contents are more related to application, while most of the courses in NTU are more about theories and don't involve many discussions. I spent lots of time on projects and the results turn out to be very satisfying. For ESA, me and my teammates design a card game about Singapore using the design techniques and softwares given in class. As if OP, we analysised the structure, culture and politics of the startup company GOGOVan. I'm very luck to be grouped with the teammates that are responsible and productive.
 			</div>
 			<div class="content__content--text">
 				<p class="subtitle">Jewel Changi Airport</p>
+				Jewel is the new mall located in the center of Changi Airport and was open on 17 April. There is an artificial waterfall in the middle of Jewel. It is surrounded by sundry plants and covered by a transparent dome. I visited there two times this week. I enjoyed the light show on the second time and it was breath-taking. This is definitely the newest must-seen attraction in Singapore.
 			</div>
 			<div class="content__content--image">
 				<div class="desktop-img horizontal-double">
@@ -33,7 +35,8 @@
 				</div>
 			</div>
 			<div class="content__content--text">
-				<p class="subtitle">Labrador Nature & Coastal Walk</p>
+				<p class="subtitle">Labrador Nature Reserve & Coastal Walk</p>
+				I remembered that it rained heavily that day, the rain was so big that I couldn't hold my umbrella steadily at all. I strated from Bukit Chermin Boardwalk, which is located at the southern coast of Singapore. Because of the crazy rain, everything I saw was grey and blurry. Some part of the boardwalk is located several meters from the coast so I literally walked on the sea, and some part of the boardwalk is in the middle of greenish wetlands. My short walking journey ends at Labrador Park MRT Station since the heavy rain. This is not a very popular site but I will definitely recommend my friends to go.
 			</div>
 			<div class="content__content--image">
 				<div class="desktop-img horizontal-double">
@@ -56,6 +59,7 @@
 			</div>
 			<div class="content__content--text">
 				<p class="subtitle">Flower Dome, Cloud Forest & Floral Fantasy</p>
+				After three months in Singapore, I was finally here! The Flower Dome and Cloud Forest were as amazing as I assumed. I like Cloud Forest a little bit more because there is a skywalk for you to walk arround the waterfall and enjoy the view from above.
 			</div>
 			<div class="content__content--image">
 				<div class="desktop-img horizontal-double">
@@ -75,6 +79,9 @@
 					<div class="swiper-button-next"></div>
     			<div class="swiper-button-prev"></div>
 				</div>
+			</div>
+			<div class="content__content--text">
+				Floral Fantasy is the new garden in Gardens by the Bays. To be honest, this one is not as impressive as Flower Dome and Cloud Forest. However, there is a 4D show that I really liked. What's so special about the 4D show is that it introduces Gardens by the Bays from the point of view of a dragonfly. It allows you to enjoy the gardens form the angles that you never experienced before.
 			</div>
 			<div class="content__content--image">
 				<div class="desktop-img horizontal-double">
@@ -105,6 +112,7 @@
 			</div>
 			<div class="content__content--text">
 				<p class="subtitle">Alexandra Arch & Handerson Waves</p>
+				Alexandra Arch and Handerson Waves are parts of the southern ridges, which is a 10-kilometer stretch of green open spaces spanning the hills of some of Singapore’s most popular parks and gardens, connected by picturesque ridges and pathways. There are gound walk and also sky walk that was built on top of the trees. If I discovered this place earlier, I will definitely visit there more often.
 			</div>
 			<div class="content__content--image">
 				<div class="desktop-img horizontal-double">
@@ -127,6 +135,7 @@
 			</div>
 			<div class="content__content--text">
 				<p class="subtitle">The Tallest Mountain in Singapore</p>
+				The mountains in Singapore are all so tiny! The tallest one is only 163.63 tall, and it took me and Kwangho less than 30 minutes to climb to the top. We ran into some monkeys and lizards along the way. After the climb, we went to 文東記 to have dinner. We ordred 麥片蝦, and it is super delicious!
 			</div>
 			<div class="content__content--image horizontal-single">
 				<div class="horizontal-single__frame img_lightbox">
@@ -136,6 +145,7 @@
 			</div>
 			<div class="content__content--text">
 				<p class="subtitle">Esplanade Concert Hall</p>
+				The program that night was Mendelssohn Vioiln Concerto, performed by SSO and Stefan Jackiw, the violin solo. Esplanade concert hall is very beautiful both from the outside and from the inside.
 			</div>
 			<div class="content__content--image horizontal-single">
 				<div class="horizontal-single__frame img_lightbox">
